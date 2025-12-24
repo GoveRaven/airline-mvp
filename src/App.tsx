315e7b085d2deb5b0af10a8787e47b5712eb4a40
Даня <daniilevgen79@gmail.com>
2025-12-24 17:54:23 +0500

    добавил компоненты и логику в App

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { Header } from './components/header';
+import { ServiceCardsList } from './components/serviceCardsList';
+import { SummaryBlock } from './components/summaryBlock';
+import { SERVICES } from './consts/serviceData';
+
+export type TChosenService = { name: string; price: number }[];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [chosenSerivce, setChosenServices] = useState<TChosenService>([]);
+
+  const handleAddService = (service: TChosenService) => {
+    setChosenServices([...chosenSerivce, ...service]);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header></Header>
+      <ServiceCardsList
+        serviceData={SERVICES}
+        onAddService={handleAddService}
+      ></ServiceCardsList>
+      <SummaryBlock chosenSerivce={chosenSerivce}></SummaryBlock>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
